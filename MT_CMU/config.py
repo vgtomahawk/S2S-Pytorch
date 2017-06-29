b38ebad1158676a6f8b2108ad1317e343e4414e9
vgtomahawk <vgtomahawk@gmail.com>
2017-06-28 22:33:13 -0400

    Updated config

--- MT_CMU/config.py
+++ MT_CMU/config.py
@@ -1,34 +1,34 @@
-emb_size=192
-layer_depth=1
-hidden_size=384
-NUM_EPOCHS=6
-start=0
-unk=1
-stop=2
-garbage=3
-use_reverse=True
-init_mixed=True
-init_enc=False
-use_attention=True
+emb_size=192 #Embedding size (assumed equal for decoder and encoder)
+layer_depth=1 #For the future, currently this is always 1 in the code.
+hidden_size=384 #Encoder and Decoder state size (assumed equal)
+NUM_EPOCHS=6 #Generally about 10 epochs are sufficient. Suggested method is to pick model with best validation perplexity.
+start=0 #start token id
+unk=1 #unk token id
+stop=2 #stop token id
+garbage=3 #garbage (PAD) token id
+use_reverse=True #Use bidirectional encoder (or not)
+init_mixed=True  #Initialize decoder state with avg of forward and backward encoder. If False, either only the forward or backward state is used for init
+init_enc=False #Use the forward encoder state for initializing decoder. If false the backward encoder (a.k.a revcoder) is used.
+use_attention=True #Turn attention on or off.
 
 optimizer_type="ADAM" # other option:"SGD"
-share_embeddings=False
-use_downstream=True
-mode="train" #train,trial,inference
-problem="SUM" #SUM/MT
-srcMasking=True
+share_embeddings=False #Share encoder and decoder embeddings
+use_downstream=True #Whether to use context vector in final vocab softmax on decoder side.
+mode="train" #train,trial,inference Note: in train mode, supply only modelName, in inference mode, supply exact checkpoint path.
+problem="SUM" #Abstractive SUM/ De-to-En-MT Note: Summarization data not available on repo.
+srcMasking=True #Whether src side masking is on (True recommended)
 
 if problem=="MT":
-    min_src_frequency=1
-    min_tgt_frequency=1
-    max_train_sentences=99412
-    MAX_SEQ_LEN=200
-    model_dir="MT_checkpoints/"
+    min_src_frequency=1 #Minimum frequency to not be UNKed on src side
+    min_tgt_frequency=1 #Minimum frequency to not be UNKed on tgt side
+    max_train_sentences=99412 #Maximum training sentences
+    MAX_SEQ_LEN=200 #Maximum Length (set to high value to make unimportant)
+    model_dir="MT_checkpoints/" #Directory to save
     #TGT_LEN_LIMIT=1000
-    normalizeLoss=False
-    decoder_prev_random=True
-    PRINT_STEP=500
-    batch_size=32
+    normalizeLoss=False #Whether to normalize loss per minibatch (recommended False)
+    decoder_prev_random=True #Not used yet.
+    PRINT_STEP=500 #Print every x minibatches
+    batch_size=32 #Batch size
 
 elif problem=="SUM":
     min_src_frequency=15
@@ -41,5 +41,5 @@ elif problem=="SUM":
     PRINT_STEP=10
     batch_size=64
 
-mem_optimize=True
-cudnnBenchmark=True
+mem_optimize=True #Memory optimizations (deleting local variables in advance etc)
+cudnnBenchmark=True #CuDNN benchmark (purpoted speedup)
